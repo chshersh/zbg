837e85f6994127d7b035599a1a362dbf3fd6a390
Dmitrii Kovanikov <kovanikov@gmail.com>
2023-03-24 18:52:15 +0000

    Use Unix.system instead of Unix.open_process_full for running shell commands

--- lib/process.ml
+++ lib/process.ml
@@ -1,17 +1,6 @@
-let print_chan (channel : in_channel) =
-  let rec loop () =
-      let () = print_endline (input_line channel) in
-      loop ()
-    in
-  try loop ()
-  with End_of_file -> ();;
-
 let proc cmd =
   Printf.eprintf "> %s\n%!" cmd;
-  let ((proc_stdout, _proc_stdin, proc_stderr) as process) = Unix.open_process_full cmd [||] in
-  print_chan proc_stdout;
-  print_chan proc_stderr;
-  let _ = Unix.close_process_full process in
+  let _exit_code = Unix.system cmd in
   ()
 
 let collect_chan (channel : in_channel) =
