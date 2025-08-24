65581510c59e5e88fc12a7a02a472171e343882c
Dmitrii Kovanikov <kovanikov@gmail.com>
2025-08-24 11:43:25 +0100

    Fix CI linters

--- lib/process.mli
+++ lib/process.mli
@@ -1,11 +1,9 @@
 val proc : string -> unit
 (** Run a given string as an external process with arguments.
 
-Redirect the called process stdout and stderr to the current process stdout.
+    Redirect the called process stdout and stderr to the current process stdout.
 
-Also print the command with a pretty prompt.
-
-*)
+    Also print the command with a pretty prompt. *)
 
 val proc_silent : string -> unit
 (** Like [proc] but doesn't print the specified command. *)
@@ -13,7 +11,6 @@ val proc_silent : string -> unit
 val proc_stdout : string -> string
 (** Run a given string as an external process with arguments.
 
-Return the process stdout as the result.
+    Return the process stdout as the result.
 
-Don't print the process output.
-*)
+    Don't print the process output. *)
